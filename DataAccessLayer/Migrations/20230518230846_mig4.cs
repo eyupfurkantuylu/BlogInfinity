5d443e1c867bb595b21f4dd68fbc6c2ed71164ba
eyuptuylu <eyupfurkantuylu@gmail.com>
2023-05-19 02:17:09 +0300

    Proje dosyası ekle.

---
+++ DataAccessLayer/Migrations/20230518230846_mig4.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BlogImage",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "BlogThumbnailImage",
+                table: "Categories");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BlogImage",
+                table: "Blogs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BlogThumbnailImage",
+                table: "Blogs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BlogImage",
+                table: "Blogs");
+
+            migrationBuilder.DropColumn(
+                name: "BlogThumbnailImage",
+                table: "Blogs");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BlogImage",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BlogThumbnailImage",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
